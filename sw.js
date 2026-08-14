b0022726b252372ca7137c02336d18c485059714
hgs3767994 <hgs3767994@gmail.com>
2026-08-15 01:36:02 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 每次更新程式碼上傳 GitHub 時，記得把這裡的版本號 +1
-const CACHE_NAME = 'zhuyin-adventure-v2.1.02';// 👈 修改這裡 (例如 v1 -> v2)
+const CACHE_NAME = 'zhuyin-adventure-v2.1.03';// 👈 修改這裡 (例如 v1 -> v2)
 
 // 離線需要快取的資源檔案清單
 const ASSETS_TO_CACHE = [
